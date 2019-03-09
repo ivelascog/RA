2bbaa370d9c15b9453d4ea44f1a6b7e579247628
GoLoT <golot@golot.es>
2019-03-09 18:08:32 +0100

    First approach to AO implementation

--- src/ao.cpp
+++ src/ao.cpp
@@ -1,4 +1,8 @@
 #include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/sampler.h>
+#include "nori/warp.h"
+#include <limits>
 
 NORI_NAMESPACE_BEGIN
 
@@ -10,17 +14,20 @@ public:
 
 	/// Compute the radiance value for a given ray. Just return green here
 	Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
-		return Color3f(0, 1, 0);
+    Intersection its;
+    if (!scene->rayIntersect(ray, its))
+      return { 0, 0, 0 };
+    Vector3f d = Warp::squareToCosineHemisphere(sampler->next2D());
+    d = its.shFrame.toWorld(d);
+
+    const Ray3f aoRay(its.p, d, 0, 0xFFFFFFFFF);
+
+    return scene->rayIntersect(aoRay) ? Color3f{0, 0, 0} : Color3f{1.0f, 1.0f, 1.0f};
 	}
 
 	/// Return a human-readable description for debugging purposes
 	std::string toString() const {
-		return tfm::format(
-			"AoIntegrator[\n"
-			"  myProperty = \"%s\"\n"
-			"]"
-			
-		);
+    return "";
 	}
 
 protected:
