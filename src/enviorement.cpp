53aaecbe6e98a9994e50054b7c1b1fdbe107391e
i.velascog <i.velascog@alumnos.urcj.com>
2019-05-18 19:11:08 +0200

    try enviorement

--- src/enviorement.cpp
+++ src/enviorement.cpp
@@ -4,7 +4,9 @@
 #include "nori/bitmap.h"
 #include <filesystem/path.h>
 #include <filesystem/resolver.h>
+#include <algorithm>
 NORI_NAMESPACE_BEGIN
+static const float InvPi = 0.31830988618379067154; static const float Inv2Pi = 0.15915494309189533577;
 class Enviorement: public Emitter {
 public:
 	Enviorement(const PropertyList &props) {
@@ -21,8 +23,19 @@ public:
 			m_filename.str());
 	}
 
+	inline float clamp(float v, float min, float max) const
+	{
+		return std::max(min, std::min(v, max));
+	}
+
 	virtual Color3f eval(const EmitterQueryRecord & lRec) const {
-		return m_texture(0,0);
+		Vector3f w = lRec.wi;
+		Point2f uv(
+			(atan2(w.x(), -w.z())) * INV_TWOPI,
+			std::acos(clamp(w.y(),-1,1)) * INV_PI
+		);
+		uv[0] = std::max(uv[0], 0.0f);
+		return m_texture(uv.y() * (m_texture.rows() - 1), uv.x() * (m_texture.cols() - 1));
 	}
 
 	virtual Color3f sample(EmitterQueryRecord & lRec, const Point2f & sample, float optional_u) const {
